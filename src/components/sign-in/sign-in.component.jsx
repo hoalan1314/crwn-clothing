fc21bdf8d967b669347dd04c041bbe469f099745
alanho <alanho@targa.com>
2020-07-19 21:52:46 +0800

    add firebase and firebase utils sample

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './sign-in.styles.scss'
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
+import {signInWithGoogle} from "../../firebase/firebase.utils";
 
 class SignIn extends React.Component {
   constructor(props) {
@@ -46,6 +47,7 @@ class SignIn extends React.Component {
         />
 
         <CustomButton type='submit'>Sign in</CustomButton>
+        <CustomButton onClick={signInWithGoogle}>Sign in With Google</CustomButton>
       </form>
     </div>
   }
