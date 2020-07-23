cd1745657cd1ba8d65ac9dbc5108fe1efb7d1ea2
alanho <alanho@targa.com>
2020-07-23 23:20:47 +0800

    add select collection reducer to return function with param(collectionId)

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import CollectionItem from "../../components/collection-item/collection-item.component";
+import {connect} from 'react-redux'
 import './collection.styles.scss'
+import {selectCollection} from "../../redux/shop/shop.selector";
 
-const CollectionPage = ({ match }) => {
-  console.log(match.params.collectionId)
+const CollectionPage = ({ collection }) => {
+  console.log(collection)
   return (
     <div className='collection-page'>
       <h2>COLLECTION PAGE</h2>
@@ -11,4 +12,8 @@ const CollectionPage = ({ match }) => {
   )
 }
 
-export default CollectionPage
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state)
+})
+
+export default connect(mapStateToProps)(CollectionPage)
