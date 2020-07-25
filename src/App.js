df8d11cd16d29a07bc582bec1161732381ce0003
alanho <alanho@targa.com>
2020-07-25 14:41:06 +0800

    remove the batch add shop data from App.js component did mount

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import CheckoutPage from "./pages/checkout/checkout.component";
-import {auth, createUserProfileDocument, addCollectionAndDocuments} from "./firebase/firebase.utils";
+import {auth, createUserProfileDocument} from "./firebase/firebase.utils";
 import {setCurrentUser} from "./redux/user/user.actions";
 import {selectCurrentUser} from "./redux/user/user.selectors";
 import {createStructuredSelector} from "reselect";
@@ -18,7 +18,7 @@ class App extends React.Component{
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    const {setCurrentUser, collectionsArray} = this.props
+    const {setCurrentUser} = this.props
 
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
@@ -31,7 +31,6 @@ class App extends React.Component{
         })
       }
       setCurrentUser(userAuth)
-      addCollectionAndDocuments('collections', collectionsArray.map(({title, items}) => ({title, items})))
     })
   }
 
