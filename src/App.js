e68254e64635a12bf782ddf8c358294a9541306a
alanho <alanho@targa.com>
2020-07-26 10:29:30 +0800

    migrate signin with email and sign in with google to sagas

--- src/App.js
+++ src/App.js
@@ -8,36 +8,11 @@ import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import CheckoutPage from "./pages/checkout/checkout.component";
-import {auth, createUserProfileDocument} from "./firebase/firebase.utils";
-import {setCurrentUser} from "./redux/user/user.actions";
 import {selectCurrentUser} from "./redux/user/user.selectors";
 import {createStructuredSelector} from "reselect";
 import {selectCollectionForPreview} from "./redux/shop/shop.selector";
 
 class App extends React.Component{
-  unsubscribeFromAuth = null
-
-  componentDidMount() {
-    const {setCurrentUser} = this.props
-
-    // this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-    //   if (userAuth) {
-    //     const userRef = await createUserProfileDocument(userAuth)
-    //     userRef.onSnapshot(snapshot => {
-    //     setCurrentUser({
-    //         id: snapshot.id,
-    //         ...snapshot.data()
-    //       })
-    //     })
-    //   }
-    //   setCurrentUser(userAuth)
-    // })
-  }
-
-  componentWillUnmount() {
-    this.unsubscribeFromAuth()
-  }
-
   render() {
     return (
       <div>
@@ -58,8 +33,4 @@ const mapStateToProps = createStructuredSelector({
   collectionsArray: selectCollectionForPreview
 })
 
-const mapDispatchToProps = dispatch => ({
-  setCurrentUser: user => dispatch(setCurrentUser(user))
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(mapStateToProps)(App);
--- src/redux/user/user.sagas.js
+++ src/redux/user/user.sagas.js
@@ -2,23 +2,55 @@ import {takeLatest, put, all, call} from 'redux-saga/effects'
 
 import UserActionTypes from "./user.types";
 import {auth, googleProvider, createUserProfileDocument} from "../../firebase/firebase.utils";
-import {googleSignInSuccess, googleSignInFailure} from "./user.actions";
+import {signInSuccess, signInFailure} from "./user.actions";
+
+export function* getSnapshotFromUserAuth(userAuth) {
+  try{
+    const userRef = yield call(createUserProfileDocument, userAuth)
+    const userSnapshot = yield userRef.get()
+    yield put(signInSuccess({id: userSnapshot.id, ...userSnapshot.data()}))
+  } catch (error) {
+    yield put(signInFailure(error.message))
+  }
+}
 
 export function* signInWithGoogle() {
   try {
     const {user} = yield auth.signInWithPopup(googleProvider)
-    const userRef = yield call(createUserProfileDocument, user)
-    const userSnapshot = yield userRef.get()
-    yield put(googleSignInSuccess({id: userSnapshot.id, ...userSnapshot.data()}))
+    yield getSnapshotFromUserAuth(user)
   } catch (error) {
-    yield put(googleSignInFailure(error.message))
+    yield put(signInFailure(error.message))
   }
 }
 
+export function* signInWithEmail({payload: {email, password}}) {
+  try {
+    const {user} = yield auth.signInWithEmailAndPassword(email, password)
+    yield getSnapshotFromUserAuth(user)
+  } catch (error) {
+    yield put(signInFailure(error.message))
+  }
+}
+
+/**
+ * Listening for GOOGLE_SIGN_IN_START
+ * @returns {Generator<<"FORK", ForkEffectDescriptor>, void, *>}
+ */
 export function* onGoogleSignInStart() {
   yield takeLatest(UserActionTypes.GOOGLE_SIGN_IN_START, signInWithGoogle)
 }
 
+/**
+ * Listening for EMAIL_SIGN_IN_START
+ * @returns {Generator<<"FORK", ForkEffectDescriptor>, void, *>}
+ */
+export function* onEmailSignInStart() {
+  yield takeLatest(UserActionTypes.EMAIL_SIGN_IN_START, signInWithEmail)
+}
+
 export function* userSagas() {
-  yield all([call(onGoogleSignInStart)])
+  yield all([
+    call(onGoogleSignInStart),
+    call(onEmailSignInStart)
+  ])
 }
