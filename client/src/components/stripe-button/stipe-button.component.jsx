eb077dedcf1e146190bde1456ad625494af48540
alanho <alanho@targa.com>
2020-08-02 09:55:59 +0800

    move cra app into client server and add express to serve the app and support stripe payment

--- client/src/components/stripe-button/stipe-button.component.jsx
+++ client/src/components/stripe-button/stipe-button.component.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
 import StripeCheckout from "react-stripe-checkout";
+import axios from 'axios'
 
 const StripeCheckoutButton = ({price}) => {
   const priceForStripe = price * 100 // in cents
   const publishableKey = 'pk_test_51H86q5Frwr3SeYjHLRXgeSoflNMnu05h3VgnvR4BXjaqr1CgFdeWUAzr2yBPG4hv0gcZvUCvkkexGBWggKQk7aSm00yCrOEmhw'
 
   const onToken = token => {
-    console.log(token)
-    alert('Payment Successful')
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    }).then(res => {
+      alert('Payment successful')
+    }).catch(error => {
+      console.log('Payment error: ', JSON.parse(error))
+      alert('There was an issue with your payment. Please sure you use the provided credit card.')
+    })
   }
 
   return (
